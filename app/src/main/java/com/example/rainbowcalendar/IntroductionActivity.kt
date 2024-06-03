6480319e5370b44d5ba74c944198ce229d8b693f
USER <nakterankanogames@gmail.com>
2024-06-03 15:22:29 +0200

    made language system better

--- app/src/main/java/com/example/rainbowcalendar/IntroductionActivity.kt
+++ app/src/main/java/com/example/rainbowcalendar/IntroductionActivity.kt
@@ -1,5 +1,6 @@
 package com.example.rainbowcalendar
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
@@ -10,11 +11,18 @@ class IntroductionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_introduction)
 
+        val sharedpref: SharedPreferences =applicationContext.getSharedPreferences("com.example.android.your_application", MODE_PRIVATE)
+        val token: String?=sharedpref.getString("token", null)
+
         val spinner = findViewById<Spinner>(R.id.themeSpinner)
         ArrayAdapter.createFromResource(this,R.array.themes_array,android.R.layout.simple_spinner_item)
             .also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spinner.adapter = adapter
+        }
+
+        if (token=="False"||token==null){
+            
         }
 
     }
