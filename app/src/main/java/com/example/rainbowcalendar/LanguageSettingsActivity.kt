4e4a4b79f753d1ce3e7ee08b6a79b1a05940571c
USER <aleksjankowiak5230@gmail.com>
2024-08-06 21:17:22 +0200

    made language system better

--- app/src/main/java/com/example/rainbowcalendar/LanguageSettingsActivity.kt
+++ app/src/main/java/com/example/rainbowcalendar/LanguageSettingsActivity.kt
@@ -3,27 +3,41 @@ package com.example.rainbowcalendar
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import java.util.Locale
 
-class LanguageSettingsActivity : AppCompatActivity() {
+class LanguageSettingsActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_language_settings)
 
-        val buttonPl = findViewById<Button>(R.id.polishBtn)
-        buttonPl.setOnClickListener{
-            changeLanguage("pl")
-            toMainScreen()
-        }
+        val langArray by lazy { resources.getStringArray(R.array.lang_array) }
+        val spinner = findViewById<Spinner>(R.id.lang_spinner)
+        ArrayAdapter.createFromResource(this,R.array.lang_array,android.R.layout.simple_spinner_item)
+            .also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spinner.adapter = adapter
+            }
 
-        val buttonEn = findViewById<Button>(R.id.engBtn)
-        buttonEn.setOnClickListener{
-            changeLanguage("eng")
-            toMainScreen()
-        }
+        spinner.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener{
+                override fun onItemSelected(parent: AdapterView<*>?,view: View?,position: Int,id: Long) {
+                    val lang: String = parent?.getItemAtPosition(position).toString()
+                    changeLanguage(langToCode(lang))
+                    Toast.makeText(this@LanguageSettingsActivity, langToCode(lang).toString(), Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    //("Not yet implemented")
+                }
+            }
     }
+
     private fun changeLanguage(lang: String){
         val config = resources.configuration
         val locale = Locale(lang)
@@ -40,5 +54,14 @@ class LanguageSettingsActivity : AppCompatActivity() {
     private fun toMainScreen(){
         val intent=Intent(this, MainActivity::class.java)
         startActivity(intent)
+    }
+    private fun langToCode(lang: String): String{
+        return when(lang){
+            "English" -> "en"
+            "Polski" -> "pl"
+            else ->{
+                "en"
+            }
+        }
     }
 }
